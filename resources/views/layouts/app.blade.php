ae6ae26e1490e78b84a2f2cb7a8967bb79f42ab3
mark <mark.refaat.ramzy@gmail.com>
2021-02-13 18:05:52 +0200

    navbar button

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,7 +45,9 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('files.index') }}">Summarize video</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
